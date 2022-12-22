c66a7443b74200bb8257cad102e8d87a21261517
kylethmas <kyle2010thomas@gmail.com>
2022-12-22 23:32:48 +0000

    links and colours

--- js/demo.js
+++ js/demo.js
@@ -5,8 +5,8 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: 'black',
+    lineColor: 'white'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
